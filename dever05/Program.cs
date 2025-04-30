a9febc221d536d2518ed18274ffcc8d06170848b
iguinho-ux <igormachadosr@gmail.com>
2025-04-30 08:58:51 -0300

    05

---
+++ dever05/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número limite: ");
+        int limite = int.Parse(Console.ReadLine());
+
+        int contador = 0;
+
+        while (contador <= limite)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 3;
+        }
+    }
+}
