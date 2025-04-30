bc7dafc457156b9f9c31f670b6ec686b20ef47de
iguinho-ux <igormachadosr@gmail.com>
2025-04-30 09:20:19 -0300

    07

---
+++ deverfinal/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+        int contador = 1;
+        int resultado = 0;
+
+        
+        while (contador <= 10)
+        {
+            resultado = resultado + numero; 
+            contador = contador + 1;
+        }
+
+        Console.WriteLine($"O número multiplicado por 10 é: {resultado}");
+    }
+}
