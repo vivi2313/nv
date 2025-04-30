506a48a9c0ad9c288698fe0e261094ad500c9ca1
iguinho-ux <igormachadosr@gmail.com>
2025-04-30 08:53:45 -0300

    dever 0444

--- dever04/Program.cs
+++ dever04/Program.cs
@@ -1,1 +1,16 @@
-﻿i
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int contador = 2;
+
+        while (contador <= N)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 2;
+        }
+    }
+}
