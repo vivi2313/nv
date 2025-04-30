6d19dde08bbdd2e9b4f7bf71dbd1841f8b1c1089
iguinho-ux <igormachadosr@gmail.com>
2025-04-30 09:07:10 -0300

    dever 07

---
+++ dever07/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+ 
+        int contador = 10;
+        Console.WriteLine("Contagem de 10 a 1:");
+        while (contador >= 1)
+        {
+            Console.WriteLine(contador);
+            contador = contador - 1;
+        }
+
+        contador = 1;
+        Console.WriteLine("\nContagem de 1 a 10:");
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 1;
+        }
+    }
+}
